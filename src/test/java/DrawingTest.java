c5473ceb607fc814d268462f99994ad6a7300d3f
frithjofhoppe <frithjof.hoppe@icloud.com>
2018-06-16 13:20:37 +0200

    Modified unit tests

--- src/test/java/DrawingTest.java
+++ src/test/java/DrawingTest.java
@@ -1,8 +1,10 @@
+import com.sun.org.apache.regexp.internal.RE;
 import forms.Circle;
 import forms.Rectangle;
 import forms.Shape;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
+import sun.plugin.dom.css.Rect;
 import sun.security.provider.SHA;
 
 import java.awt.*;
@@ -23,6 +25,18 @@ class DrawingTest {
 
         Drawing d = new Drawing(shapes);
         d.setGraphics(g);
+
+        assertEquals(d.shapes.get(0) instanceof Circle, true);
+        assertEquals(((Circle)d.shapes.get(0)).posX1, 10);
+        assertEquals(((Circle)d.shapes.get(0)).posY1, 10);
+        assertEquals(((Circle)d.shapes.get(0)).getRadius(), 5);
+
+        assertEquals(d.shapes.get(1) instanceof Rectangle, true);
+        assertEquals(((Rectangle)d.shapes.get(1)).posX1, 20);
+        assertEquals(((Rectangle)d.shapes.get(1)).posY1, 20);
+        assertEquals(((Rectangle)d.shapes.get(1)).getLength(), 5);
+        assertEquals(((Rectangle)d.shapes.get(1)).getWidth(), 5);
+
         d.drawShapes();
         InOrder order = inOrder(g);
         order.verify(g).fillOval(10,10,10,10);
